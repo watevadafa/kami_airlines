0d344e9dd0ff1134fcbf3e1313e56b891cf86eed
Tushar Sarang <tushar@theprocedure.in>
2024-03-15 00:09:02 +0300

    adds func for airplane related calculations

--- airplanes/utils.py
+++ airplanes/utils.py
@@ -1,5 +1,41 @@
+""" Provdies neccessary utility functions for calculating propeties of airplanes"""
+
 import math
 
+from airplanes.constants import (
+    FUEL_TANK_CAPACITY_MULTIPLIER,
+    AIRPLANE_FUEL_CONSUMPTION_MULTIPLIER,
+    PER_PASSENGER_FUEL_CONSUMPTION_MULTIPLIER,
+)
 
-def get_log(value):
-    return math.log(value)
+
+def calculate_fuel_tank_capacity_in_liters(airplane_id: int) -> int:
+    """Returns the airplane's fuel tank capacity in liters"""
+    return airplane_id * FUEL_TANK_CAPACITY_MULTIPLIER
+
+
+def calculate_fuel_consumption_rate_per_minute(
+    airplane_id: int, number_of_passengers: int
+) -> float:
+    """Returns the airplane's fuel consumption rate in liters per minute"""
+
+    airplane_fuel_consumption_per_minute: float = (
+        math.log(airplane_id) * AIRPLANE_FUEL_CONSUMPTION_MULTIPLIER
+    )
+    per_passenger_fuel_consumption_per_minute: float = (
+        number_of_passengers * PER_PASSENGER_FUEL_CONSUMPTION_MULTIPLIER
+    )
+
+    return (
+        airplane_fuel_consumption_per_minute
+        + per_passenger_fuel_consumption_per_minute
+    )
+
+
+def calculate_maximum_flight_duration_in_minutes(
+    fuel_tank_capacity_in_liters: int,
+    fuel_consumption_rate_per_minute: float,
+) -> float:
+    """Returns the maximum flight duration in minutes"""
+
+    return fuel_tank_capacity_in_liters / fuel_consumption_rate_per_minute
